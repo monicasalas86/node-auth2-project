16ebff91a3e6052de770c90248fc76f7789df20a
monicasalas86 <monicsalas167@gmail.com>
2021-11-10 00:06:30 -0600

    findById function working and get user by id endpoint working without middleware

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -17,7 +17,7 @@ const { restricted, only } = require("../auth/auth-middleware.js");
     }
   ]
  */
-router.get("/", (req, res, next) => { // done for you
+router.get("/", restricted, (req, res, next) => { // done for you
   Users.find()
     .then(users => {
       res.json(users);
@@ -40,7 +40,7 @@ router.get("/", (req, res, next) => { // done for you
     }
   ]
  */
-router.get("/:user_id", only('admin'), (req, res, next) => { // done for you
+router.get("/:user_id", restricted, only('admin'), (req, res, next) => { // done for you
   Users.findById(req.params.user_id)
     .then(user => {
       res.json(user);
