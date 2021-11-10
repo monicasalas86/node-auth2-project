f0ffb186b95b045406b5a8c69ac2dc796b597326
monicasalas86 <monicsalas167@gmail.com>
2021-11-10 00:38:14 -0600

    restricted middleware working

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,4 +1,5 @@
-const { JWT_SECRET } = require("../secrets"); // use this secret!
+const { JWT_SECRET } = require("../secrets") // use this secret!
+const jwt = require('jsonwebtoken')
 
 const restricted = (req, res, next) => {
   /*
@@ -16,6 +17,26 @@ const restricted = (req, res, next) => {
 
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
+  const token = req.headers.authorization
+  console.log(req.headers)
+
+  if (!token) {
+    return next({
+      status: 401,
+      message: 'Token required'
+    })
+  }
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return next({
+        status: 401,
+        message: 'Token invalid'
+      })
+    }
+    req.decodedJwt = decoded
+    console.log(decoded)
+    next()
+  })
 }
 
 const only = role_name => (req, res, next) => {
